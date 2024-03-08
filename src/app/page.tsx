a4250266a28ae58af676eceeffcd32f7fa7689e5
tiffany may <tiffyzsmile@gmail.com>
2024-03-08 15:37:01 -0800

    create home page content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,6 +7,7 @@ import { generateClient } from "aws-amplify/api";
 // having it in main layout.tsx should be enough :/
 import { Amplify } from "aws-amplify";
 import awsconfig from "../aws-exports";
+
 Amplify.configure(awsconfig, { ssr: true });
 const client = generateClient();
 
@@ -16,7 +17,34 @@ const HomePage = async () => {
   });
 
   return (
-    <main className={styles.main}>
+    <section className={styles.homepage}>
+      <h1>Hi, my name is Tiffany May, I am a web developer.</h1>
+      <img src="/images/cartoonImageHome.gif" alt="Tiffany as a Cartoon" />
+      <p>
+        I have a passion for what I do and am looking for a challenging
+        environment to continue learning and growing from.&nbsp;
+      </p>
+      <p>
+        My dream environment would include some of the following
+        characteristics:
+      </p>
+      <ul>
+        <li>
+          Values all roles in the concept, design, development and testing
+          process
+        </li>
+        <li>
+          Continuously raising the bar and taking pride in all work performed
+        </li>
+        <li>
+          A true dedication and understanding of Web Standards and Best Practice
+        </li>
+        <li>
+          A talented team of individuals that share a common passion for Web
+          Technologies
+        </li>
+      </ul>
+      <hr />
       <ul>
         {data.listProjects.items.map((project) => {
           return (
@@ -27,7 +55,7 @@ const HomePage = async () => {
           );
         })}
       </ul>
-    </main>
+    </section>
   );
 };
 
