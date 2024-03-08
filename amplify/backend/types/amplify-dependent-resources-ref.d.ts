228a399b14700bc70dc00d47e5433604c4c914f8
tiffany may <tiffyzsmile@gmail.com>
2024-03-08 11:22:56 -0800

    add prod backend from tiffanylea-com

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,56 @@
+export type AmplifyDependentResourcesAttributes = {
+  "analytics": {
+    "tiffanyleacom": {
+      "Id": "string",
+      "Region": "string",
+      "appName": "string"
+    }
+  },
+  "api": {
+    "tiffanyleacom": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string"
+    }
+  },
+  "auth": {
+    "tiffanyleacom0b033fd5": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "AppClientSecret": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    },
+    "userPoolGroups": {
+      "adminGroupRole": "string"
+    }
+  },
+  "function": {
+    "S3Trigger52e00513": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    }
+  },
+  "hosting": {
+    "S3AndCloudFront": {
+      "CloudFrontDistributionID": "string",
+      "CloudFrontDomainName": "string",
+      "CloudFrontOriginAccessIdentity": "string",
+      "CloudFrontSecureURL": "string",
+      "HostingBucketName": "string",
+      "Region": "string",
+      "S3BucketSecureURL": "string",
+      "WebsiteURL": "string"
+    }
+  },
+  "storage": {
+    "tiffanyleacomcontent": {
+      "BucketName": "string",
+      "Region": "string"
+    }
+  }
+}
