9cd23d28d837276da5c53fbb090989c7623770fb
tiffany may <tiffyzsmile@gmail.com>
2024-04-06 13:44:18 -0700

    update build output - test

--- src/app/portfolio/[catId]/[tagId]/page.tsx
+++ src/app/portfolio/[catId]/[tagId]/page.tsx
@@ -1,21 +1,6 @@
 import React from "react";
 import Portfolio from "@/components/Portfolio";
 import { getProjects } from "@/data/getProjects";
-import { getGroupedTags } from "@/data/getTags";
-
-export async function generateStaticParams({
-  params: { catId },
-}: {
-  params: { catId: string };
-}) {
-  const tags = await getGroupedTags().then((groupedTags) => {
-    return groupedTags[catId];
-  });
-
-  return tags.map((tag) => ({
-    tagId: tag.id,
-  }));
-}
 
 type Props = {
   params: { catId: string; tagId: string };
