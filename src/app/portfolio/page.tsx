71da9957f3ef70b4e636e7a20397876664a0f3ee
tiffany may <tiffyzsmile@gmail.com>
2024-04-07 12:08:00 -0700

    build test

--- src/app/portfolio/page.tsx
+++ src/app/portfolio/page.tsx
@@ -2,8 +2,6 @@ import React from "react";
 import Portfolio from "@/components/Portfolio";
 import { getProjects } from "@/data/getProjects";
 
-export const dynamic = "force-static";
-
 const PortfolioPage = async () => {
   const projects = await getProjects({}).then((projects) =>
     projects.filter((project) => project.display),
