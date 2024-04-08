bb8c4468495a066e677fac2bf423848c4d35f680
tiffany may <tiffyzsmile@gmail.com>
2024-04-08 13:54:43 -0700

    removing unused font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,9 @@
 import type { Metadata } from "next";
-import { Lato } from "next/font/google";
 import ConfigureAmplifyClientSide from "../components/ConfigureAmplifyClientSide";
 import Header from "../components/Header";
 import styles from "./layout.module.scss";
 import "./globals.scss";
 import Footer from "@/components/Footer";
-
-const inter = Lato({ subsets: ["latin"], weight: ["400", "700"] });
 
 export const metadata: Metadata = {
   title: "Tiffany Lea May",
@@ -21,7 +18,7 @@ const RootLayout = ({
 }>) => {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body>
         <ConfigureAmplifyClientSide />
         <div className={styles.page}>
           <Header />
