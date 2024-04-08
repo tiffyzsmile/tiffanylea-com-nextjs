caed351237a2ee525259788745779476005bfe3a
tiffany may <tiffyzsmile@gmail.com>
2024-04-08 11:59:40 -0700

    remove dead code

--- src/data/categories.ts
+++ src/data/categories.ts
@@ -1,48 +1,24 @@
-/* eslint-disable */
-
-// const categories = [
-//   'Analytics',
-//   'AWS',
-//   'CMS',
-//   'Compliance',
-//   'Data',
-//   'Devops',
-//   'E-Commerce',
-//   'Framework',
-//   'Industry',
-//   'Language',
-//   'Library',
-//   'Project Type',
-//   'Role',
-//   'Server',
-//   'Software',
-//   'Tooling',
-//   'User Interface (UI)',
-//   'User Experience (UX)',
-//   'Other'
-// ];
-
 const categories = {
-  analytics: 'Analytics',
-  aws: 'AWS',
-  cms: 'CMS',
+  analytics: "Analytics",
+  aws: "AWS",
+  cms: "CMS",
   // compliance: 'Compliance',
-  data: 'Data',
+  data: "Data",
   // devops: 'Devops',
-  'e-commerce': 'E-Commerce',
-  framework: 'Framework',
-  industry: 'Industry',
-  language: 'Language',
-  'project-type': 'Project Type',
+  "e-commerce": "E-Commerce",
+  framework: "Framework",
+  industry: "Industry",
+  language: "Language",
+  "project-type": "Project Type",
   // platform: 'Platform',
-  role: 'Role',
+  role: "Role",
   // server: 'Server',
   // software: 'Software',
-  sdlc: 'SDLC',
-  tooling: 'Tooling',
+  sdlc: "SDLC",
+  tooling: "Tooling",
   // 'user-interface-ui': 'User Interface (UI)',
-  'user-experience-ux': 'User Experience',
-  year: 'Year'
+  "user-experience-ux": "User Experience",
+  year: "Year",
   // other: 'Other'
 };
 export default categories;
